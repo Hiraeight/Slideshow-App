911c2f00736f947b7078503757db1b41aa5d591b
unknown <johnpatrickdfelicia.com>
2024-09-25 19:21:26 +0800

    Apllied useState. Added state tracker for the current slide index. Declared the buttons, gave handler functions

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Slides({slides}) {
+
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    const handleNext = () => {
+        if (currentSLide > slides.length -1) {
+            setCurrentSlide(currentSlide + 1);
+        }
+    };
+
+    const handlePrev = () => {
+        if (currentSlide > 0) {
+            setCurrentSlide(currentSlide - 1);
+        }
+    };
+
+    const handleRestart = () => {
+        setCurrentSlide(0);
+    };
 
     return (
         <div>
             <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
+                <button
+                data-testid="button-restart"
+                className="small outlined"
+                >
+                    Restart
+                </button>
+                <button
+                data-testid="button-prev"
+                className="small"
+                >
+                    Prev
+                </button>
+                <button
+                data-testid="button-next"
+                className="small"
+                >
+                    Next
+                </button>
             </div>
             <div id="slide" className="card text-center">
                 <h1 data-testid="title">Slide Title Here</h1>
