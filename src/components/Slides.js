b7a2124db535e3d9a71238d32e85a4caa4e1540b
unknown <johnpatrickdfelicia.com>
2024-09-25 19:34:16 +0800

    Setting up onClick event. Applied disable functionality when currentSlide is the first slide. Sets up Title and Text for each slides

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -5,7 +5,7 @@ function Slides({slides}) {
     const [currentSlide, setCurrentSlide] = useState(0);
 
     const handleNext = () => {
-        if (currentSLide > slides.length -1) {
+        if (currentSlide < slides.length -1) {
             setCurrentSlide(currentSlide + 1);
         }
     };
@@ -26,25 +26,31 @@ function Slides({slides}) {
                 <button
                 data-testid="button-restart"
                 className="small outlined"
+                onClick={handleRestart}
+                disabled={currentSlide === 0}
                 >
                     Restart
                 </button>
                 <button
                 data-testid="button-prev"
                 className="small"
+                onClick={handlePrev}
+                disabled={currentSlide === 0}
                 >
                     Prev
                 </button>
                 <button
                 data-testid="button-next"
                 className="small"
+                onClick={handleNext}
+                disabled={currentSlide === slides.length -1}
                 >
                     Next
                 </button>
             </div>
             <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
+                <h1 data-testid="title">{slides[currentSlide].title}</h1>
+                <p data-testid="text">{slides[currentSlide].text}</p>
             </div>
         </div>
     );
